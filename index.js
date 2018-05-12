61a2182f34f55e325df739939295d9141b6e275a
Jono Hill <jonathan@hillnz.com>
2018-05-12 15:38:19 +1200

    Use async/await

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var Service, Characteristic, Accessory;
 var WirelessTagAccessory;
 
 // Handle registration with homebridge
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     Accessory = homebridge.hap.Accessory;
-    
+
     WirelessTagAccessory = require('./accessories/wireless_tag')(Accessory, Service, Characteristic);
-    
+
     homebridge.registerPlatform("homebridge-wireless-sensor-tag", "wireless-sensor-tag", WirelessTagPlatform);
 }
 
@@ -26,85 +26,78 @@ function WirelessTagPlatform(log, config) {
 }
 
 WirelessTagPlatform.prototype = {
-    reloadData: function(callback) {
+    reloadData: function (callback) {
         var that = this;
-        var foundAccessories = [];
         
-        wirelesstags.getTagList(this.token, function(devices) {
+        (async function() {
+            var foundAccessories = [];
+            let devices = await wirelesstags.getTagList(this.token);
             if (devices && devices instanceof Array) {
                 for (var i = 0; i < devices.length; i++) {
                     var device = devices[i];
                     var config = {};
                     var accessory = undefined;
                     var emulateThermostat = that.emulateThermostats.indexOf(device.name) >= 0;
-
-                    var checkDevice = function () {
-                        // Device already added, so just load data
-                        if (that.deviceLookup[device.uuid]) {
-                            accessory = that.deviceLookup[device.uuid];
-                            accessory.device = device;
-                            accessory.config = config;
-                            accessory.loadData();
-                        }
-                        else {
-                            accessory = new WirelessTagAccessory(that, device, config, {
-                                motionSensor: that.motionSensors.indexOf(device.name) >= 0,
-                                contactSensor: that.contactSensors.indexOf(device.name) >= 0,
-                                emulateThermostat: emulateThermostat
-                            });
-
-                            // Device successfully added
-                            if (accessory !== undefined) {
-                                that.log("Device added - " + device.uuid);
-                                that.deviceLookup[device.uuid] = accessory;
-                                foundAccessories.push(accessory);
-                            }
-                            // Unknown device - skip
-                            else {
-                                that.log("Device skipped - " + device.uuid);
-                            }
-                        }
-                    }
-                    
+    
                     if (emulateThermostat) {
                         // Supplement additional config data
-                        wirelesstags.loadTempSensorConfig(device.slaveId, that.token, function (sensorConfig) {
-                            if (sensorConfig) {
-                                config = sensorConfig;
-                            } else {
-                                that.log("loadTempSensorConfig - error getting tag config for " + device.uuid);
-                            }
-                            checkDevice();
-                        });
-                    } else {
-                        checkDevice();
+                        config = await wirelesstags.loadTempSensorConfig(device.slaveId, that.token);
+                        if (!config) {
+                            that.log("loadTempSensorConfig - error getting tag config for " + device.uuid);
+                            config = {};
+                        }
+                    }          
+    
+                    // Device already added, so just load data
+                    if (that.deviceLookup[device.uuid]) {
+                        accessory = that.deviceLookup[device.uuid];
+                        accessory.device = device;
+                        accessory.config = config;
+                        accessory.loadData();
                     }
-
-
-                }
-                
-                if (callback) {
-                    callback(foundAccessories);
+                    else {
+                        accessory = new WirelessTagAccessory(that, device, config, {
+                            motionSensor: that.motionSensors.indexOf(device.name) >= 0,
+                            contactSensor: that.contactSensors.indexOf(device.name) >= 0,
+                            emulateThermostat: emulateThermostat
+                        });
+    
+                        // Device successfully added
+                        if (accessory !== undefined) {
+                            that.log("Device added - " + device.uuid);
+                            that.deviceLookup[device.uuid] = accessory;
+                            foundAccessories.push(accessory);
+                        }
+                        // Unknown device - skip
+                        else {
+                            that.log("Device skipped - " + device.uuid);
+                        }
+                    }
                 }
             }
             else {
                 that.log("getTagList - error getting tag list");
-                if (callback) {
-                    callback(foundAccessories);
-                }
+            }
+            return foundAccessories;
+
+        })()
+        .then(result => {
+            if (callback) {
+                callback(result);
             }
         });
+
     },
-    accessories: function(callback) {
+    accessories: function (callback) {
         this.log("Fetching Wireless Tags");
         var that = this;
         var foundAccessories = [];
         this.deviceLookup = [];
-        
-        this.reloadData(function(foundAccessories) {
+
+        this.reloadData(function (foundAccessories) {
             callback(foundAccessories);
         });
-        
+
         if (that.queryFrequency === undefined || that.queryFrequency < 5000) {
             that.log('Invalid query frequency; setting to 20000ms default');
             that.queryFrequency = 20000;
